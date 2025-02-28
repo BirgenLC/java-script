260276be4ff2739a7c045fb3b09e098ee6afb378
Birgen <birgen156@gmail.com>
2025-02-27 23:02:41 -0800

    java script

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+//Data store in variables can be of different formats
+//in js data can be numbers, strings,boolean,undefined ,null ,arrays ,objects
+
+let age = 50; //number
+let price = 890.45; //number
+let email = "albert@eldohub.co.ke"; //string
+let isKenyan = false; //boolean
+let isBrandNew = true; //boolean
+let username; //undefined
+let totalScore = null; //null
+
+let learners = ["Brigid", "Tiffany", "Daisy", "Isaiah"];
+let randomData = ["Albert", 6783, true, "amazing", 45];
+let car = {
+  brand: "Toyota",
+  model: "Probox",
+  yom: 2018,
+  isKenyanUsed: false,
+  mileage: 23005,
+  colors: ["white", "green", "orange"],
+}; //object
