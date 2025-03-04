1df202a7660ac928c7b3e5426761b19bb650509f
Birgen <birgen156@gmail.com>
2025-03-04 00:16:02 -0800

    objects

---
+++ functions.js
@@ -0,0 +1,76 @@
+// Function -  is a reusable block of code {} . Functions create their own scope for variables.
+
+// defining/creating a funciton ---  regular functions vs arrow functions
+
+function doSomething() {
+  console.log("Block of code to be executed ");
+  console.log("Regular function");
+}
+
+const doSomethingElse = () => {
+  console.log("Block of code to be executed ");
+  console.log("Arrow function");
+};
+
+// using/calling/invoking functions --  as many times as we want
+
+doSomething();
+doSomethingElse();
+doSomethingElse();
+doSomethingElse();
+doSomething();
+
+let username = "albert";
+
+// write a function to convert money to different currencies depending on the country provided
+function convertCurrency(amount, country) {
+  // amount and country are parameters/arguments  for this cunftion - they are variables only accessible inside this function's block/scope
+  // any other variable created within this block of code is only accesible here.
+
+  console.log("Hello my friend from " + country);
+  if (country == "USA") {
+    console.log("Amount is: " + amount / 129 + "USD");
+  } else if (country == "uganda") {
+    console.log("Amount is: " + amount * 28 + "UGSH");
+  } else if (country == "japan") {
+    console.log("Amount is: " + amount * 1.17 + "YEN");
+  } else {
+    console.log("Amount is: " + amount + "KSH");
+  }
+  console.log(username);
+}
+//calling the function
+convertCurrency(2000, "USA");
+convertCurrency(2000, "uganda");
+convertCurrency(2000, "japan");
+convertCurrency(543, "japan");
+
+// global scope vs block scope
+
+// Write a function that takes a number as an argument and checks if the number is even or odd
+
+function checkIfEvenOrOdd(number) {
+  if (number % 2 == 0) {
+    console.log("The number" + number + "is Even");
+  } else {
+    console.log("The number" + number + "Is Odd");
+  }
+}
+
+checkIfEvenOrOdd(90);
+checkIfEvenOrOdd(901);
+checkIfEvenOrOdd(875);
+checkIfEvenOrOdd(876);
+
+// write a function that takes in an array of names, loops through the names changing them to all uppercase
+function changeNamesToUpperCase(names) {
+  let finalArray = [];
+  for (let index = 0; index < names.length; index = index + 1) {
+    finalArray.push(names[index].toUpperCase());
+  }
+  console.log(finalArray);
+}
+
+changeNamesToUpperCase(["albert", "nobert"]);
+
+// strings and numbers and arrays
---
+++ objects.js
@@ -0,0 +1,179 @@
+// almost everything in javascript is an object
+
+let car = {
+  brand: "Toyota",
+  model: "Probox",
+  yom: 2018,
+  isKenyanUsed: false,
+  mileage: 23005,
+  colors: ["white", "green", "orange"],
+};
+
+// accessing data in an object
+// e.g. log the year of make for this car
+
+console.log(car.yom); // using the dot notation - 2018
+
+// log the second color of the car -- green
+
+const people = [
+  {
+    name: "Albert",
+    age: 50,
+    isKenyan: true,
+    schools: ["Meteitei", "JKUAT", "MKU"],
+    work: {
+      company: "Eldohub",
+      role: "Instructor",
+      since: 2022,
+    },
+    siblings: [
+      { name: "Alex", age: 31 },
+      { name: "Alfred", age: 25 },
+      { name: "Allan", age: 29 },
+    ],
+    height: 5.9,
+  },
+  {
+    name: "Beatrice",
+    age: 35,
+    isKenyan: false,
+    schools: ["Hillcrest", "Moi University"],
+    work: {
+      company: "Technovation",
+      role: "Software Engineer",
+      since: 2018,
+    },
+    siblings: [],
+    height: 5.5,
+  },
+  {
+    name: "Charles",
+    age: 42,
+    isKenyan: true,
+    schools: ["Maranda", "University of Nairobi"],
+    work: {
+      company: "Acme Corporation",
+      role: "Manager",
+      since: 2015,
+    },
+    siblings: [
+      { name: "Christine", age: 38 },
+      { name: "Caleb", age: 35 },
+    ],
+    height: 6.1,
+  },
+  {
+    name: "Diana",
+    age: 18,
+    isKenyan: false,
+    schools: ["St. Mary's", "Strathmore University"],
+    work: {
+      company: "FinTech Ltd",
+      role: "Data Analyst",
+      since: 2020,
+    },
+    siblings: [{ name: "David", age: 30 }],
+    height: 5.7,
+  },
+  {
+    name: "Edith",
+    age: 65,
+    isKenyan: true,
+    schools: ["Ki̅rei", "Egerton University"],
+    work: {
+      company: "Retired",
+      role: "Teacher",
+      since: 1980,
+    },
+    siblings: [],
+    height: 5.4,
+  },
+  {
+    name: "Felix",
+    age: 19,
+    isKenyan: true,
+    schools: ["Butere", "Maseno University"],
+    work: {
+      company: "Freelancer",
+      role: "Web Developer",
+      since: 2023,
+    },
+    siblings: [{ name: "Fiona", age: 17 }],
+    height: 5.1,
+  },
+  {
+    name: "Gladys",
+    age: 48,
+    isKenyan: false,
+    schools: ["Loreto", "Kenyatta University"],
+    work: {
+      company: "Hospital",
+      role: "Doctor",
+      since: 2000,
+    },
+    siblings: [
+      { name: "George", age: 52 },
+      { name: "Gloria", age: 45 },
+    ],
+    height: 5.8,
+  },
+  {
+    name: "Henry",
+    age: 32,
+    isKenyan: true,
+    schools: ["Nyandarua", "University of Eldoret"],
+    work: {
+      company: "Government",
+      role: "Lawyer",
+      since: 2019,
+    },
+    siblings: [],
+    height: 6.2,
+  },
+];
+
+//   print/log the work place for the thrid person
+
+// log the name of Felix's sibling
+const Felix = people.find((person) => person.name === "Felix");
+console.log(Felix.siblings[0].name);
+
+console.log(people[5].siblings[0].name);
+
+// log the second school that Diana went to
+// the age of the youngest person
+// log the number of Glady's siblings
+function findYoungestPerson(peopleList) {
+  let youngestAge = peopleList[0].age; // an assumption
+  for (let index = 0; index < peopleList.length; index++) {
+    if (peopleList[index].age < youngestAge) {
+      youngestAge = peopleList[index].age;
+    }
+  }
+  console.log(youngestAge);
+  const youngestPerson = peopleList.find(
+    (person) => person.age === youngestAge
+  ).name;
+  console.log(youngestPerson);
+}
+findYoungestPerson(people);
+
+const username = "kipchirchir";
+
+console.log(username.endsWith("k"));
+console.log(username.endsWith("r"));
+console.log(username.toUpperCase());
+console.log(username.charAt(0));
+
+console.log(username.replace("kip", "chep"));
+
+const price = 900.878369026;
+console.log(price.toFixed(2));
+
+//Number
+//JS global objects ---string,number,array,objects,math,date...provide properties and methods(funtions)to help manipulate data
+const colors = ["red", "green"];
+colors.push("yellow");
+console.log(colors);
+console.log(colors.reverse());
